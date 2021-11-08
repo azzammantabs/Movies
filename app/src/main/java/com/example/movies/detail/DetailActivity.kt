44504e547f231867b49ac84230eafe7704577a8b
Azzam <azzamcis123@gmail.com>
2021-11-08 00:44:09 -0800

    third, api youtube video

--- app/src/main/java/com/example/movies/detail/DetailActivity.kt
+++ app/src/main/java/com/example/movies/detail/DetailActivity.kt
@@ -1,15 +1,13 @@
 package com.example.movies.detail
 
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import android.widget.MediaController
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
-import com.example.movies.R
 import com.example.movies.adapter.ReviewAdapter
 import com.example.movies.databinding.ActivityDetailBinding
 import com.example.movies.helper.Constant.Companion.BASE_URL_IMAGE
@@ -17,6 +15,8 @@ import com.example.movies.helper.Constant.Companion.BASE_URL_YOUTUBE
 import com.example.movies.model.Movies
 import com.example.movies.model.Reviewer
 import com.example.movies.model.Video
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
 
 
 class DetailActivity : AppCompatActivity() {
@@ -92,11 +92,11 @@ class DetailActivity : AppCompatActivity() {
     private fun setData(movies: Movies) {
         Glide.with(this)
             .load("$BASE_URL_IMAGE/${movies.backdrop_path}")
-            .error(R.drawable.ic_outline_image_24)
+            .error(com.example.movies.R.drawable.ic_outline_image_24)
             .into(binding.ivMoviesDetail)
         Glide.with(this)
             .load("$BASE_URL_IMAGE/${movies.poster_path}")
-            .error(R.drawable.ic_outline_image_24)
+            .error(com.example.movies.R.drawable.ic_outline_image_24)
             .into(binding.ivMoviesPosterDetail)
 
         binding.tvTitleDetail.text = movies.original_title
@@ -116,11 +116,42 @@ class DetailActivity : AppCompatActivity() {
     }
 
     private fun playVideo(video: Video) {
-        val mediaController = MediaController(this)
-        binding.vvTrailerDetail.setMediaController(mediaController)
-        val path = "$BASE_URL_YOUTUBE${video.key}"
-        val uid = Uri.parse(path)
-        binding.vvTrailerDetail.setVideoURI(uid)
-        binding.vvTrailerDetail.start()
+//        val mediaController = MediaController(this)
+//        binding.vvTrailerDetail.setMediaController(mediaController)
+        val apiKey = "$BASE_URL_YOUTUBE"
+//        val uid = Uri.parse(path)
+//        binding.vvTrailerDetail.setVideoURI(uid)
+//        binding.vvTrailerDetail.start()
+        // Get reference to the view of Video player
+        // Get reference to the view of Video player
+
+        binding.vvTrailerDetail.initialize(
+            apiKey,
+            object : YouTubePlayer.OnInitializedListener {
+                // Implement two methods by clicking on red
+                // error bulb inside onInitializationSuccess
+                // method add the video link or the playlist
+                // link that you want to play In here we
+                // also handle the play and pause
+                // functionality
+                override fun onInitializationSuccess(
+                    provider: YouTubePlayer.Provider,
+                    youTubePlayer: YouTubePlayer, b: Boolean
+                ) {
+                    youTubePlayer.loadVideo(video.key)
+                    youTubePlayer.play()
+                }
+
+                // Inside onInitializationFailure
+                // implement the failure functionality
+                // Here we will show toast
+                override fun onInitializationFailure(
+                    provider: YouTubePlayer.Provider,
+                    youTubeInitializationResult: YouTubeInitializationResult
+                ) {
+                    Toast.makeText(applicationContext, "Video player Failed", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            })
     }
 }
